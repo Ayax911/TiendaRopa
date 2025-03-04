8d27fcaee48828a69c64ca605b69600447b4565a
Julian Ojeda <j.mast8924@gmail.com>
2025-03-04 11:52:04 -0500

    Verificacion en Universidad

--- TiendaRopa/Program.cs
+++ TiendaRopa/Program.cs
@@ -5,74 +5,68 @@ Console.WriteLine("Hello, World!");
 
 var lista_clientes = new List <Clientes>();
 
-lista_clientes.Add (new Clientes() { IdCliente = 1, Nombre = "Carlos", Apellido = "Romero", Telefono = "3157894562" });
-lista_clientes.Add(new Clientes() { IdCliente = 2, Nombre = "Marco", Apellido = "Polo", Telefono = "3164568582" });
-lista_clientes.Add(new Clientes() { IdCliente = 3, Nombre = "Alan", Apellido = "Turing", Telefono = "3125469887" });
-lista_clientes.Add(new Clientes() { IdCliente = 4, Nombre = "Ada", Apellido = "Lovelace", Telefono = "3225648213" });
-lista_clientes.Add(new Clientes() { IdCliente = 5, Nombre = "Grace", Apellido = "Hopper", Telefono = "3124567898" });
-lista_clientes.Add(new Clientes() { IdCliente = 6, Nombre = "Dorothy", Apellido = "Sayers", Telefono = "3124587951" });
+lista_clientes.Add (new Clientes() { IdClientes = 1, Cedula = "111", Nombre = "Carlos Romero", Telefono = "3157894562" });
+lista_clientes.Add(new Clientes() { IdClientes = 2, Cedula = "222", Nombre = "Marco Polo", Telefono = "3164568582" });
+lista_clientes.Add(new Clientes() { IdClientes = 3, Cedula = "333", Nombre = "Alan Turing", Telefono = "3125469887" });
+lista_clientes.Add(new Clientes() { IdClientes = 4, Cedula = "444", Nombre = "Ada Lovelace", Telefono = "3225648213" });
+
 
 
 var lista_sucursales = new List<Sucursales>();
 
-lista_sucursales.Add(new Sucursales() { IdSucursales = 1, Nombre = "A1"});
-lista_sucursales.Add(new Sucursales() { IdSucursales = 2, Nombre = "A2" });
-lista_sucursales.Add(new Sucursales() { IdSucursales = 3, Nombre = "A3" });
-lista_sucursales.Add(new Sucursales() { IdSucursales = 4, Nombre = "A4" });
+lista_sucursales.Add(new Sucursales() { IdSucursales = 1, Nombre = "A1" , Direccion = "Calle 1"});
+lista_sucursales.Add(new Sucursales() { IdSucursales = 2, Nombre = "A2" , Direccion = "Calle 2 " });
+lista_sucursales.Add(new Sucursales() { IdSucursales = 3, Nombre = "A3" , Direccion = "Calle 3" });
+lista_sucursales.Add(new Sucursales() { IdSucursales = 4, Nombre = "A4" , Direccion = "Calle 4" });
 
 
 var lista_metodospagos = new List<MetodosPagos>();
 
-lista_metodospagos.Add(new MetodosPagos() { IdMetodoPago = 1, Nombre = "Credito"});
-lista_metodospagos.Add(new MetodosPagos() { IdMetodoPago = 2, Nombre = "Credito" });
-lista_metodospagos.Add(new MetodosPagos() { IdMetodoPago = 3, Nombre = "Credito" });
+lista_metodospagos.Add(new MetodosPagos() { IdMetodosPagos = 1, Nombre = "Credito", Estado = true});
+lista_metodospagos.Add(new MetodosPagos() { IdMetodosPagos = 2, Nombre = "Debito" , Estado = true });
+lista_metodospagos.Add(new MetodosPagos() { IdMetodosPagos = 3, Nombre = "Efectivo" , Estado = true });
 
 
 var lista_lugares = new List<Lugares>();
 
-lista_lugares.Add(new Lugares() { IdLugar = 1, Nombre = "San Antonio" });
-lista_lugares.Add(new Lugares() { IdLugar = 2, Nombre = "Poblado" });
-lista_lugares.Add(new Lugares() { IdLugar = 3, Nombre = "Las Americas" });
+lista_lugares.Add(new Lugares() { IdLugares = 1, Nombre = "San Antonio", CodigoPostal = "5001" });
+lista_lugares.Add(new Lugares() { IdLugares = 2, Nombre = "Poblado", CodigoPostal = "5002" });
+lista_lugares.Add(new Lugares() { IdLugares = 3, Nombre = "Las Americas", CodigoPostal = "5003" });
 
 
 var lista_marcas = new List<Marcas>();
 
-lista_marcas.Add(new Marcas() { IdMarca = 1, Nombre = "Arturo Calle" });
-lista_marcas.Add(new Marcas() { IdMarca = 2, Nombre = "Patprimo" });
-lista_marcas.Add(new Marcas() { IdMarca = 3, Nombre = "Estudio F" });
-lista_marcas.Add(new Marcas() { IdMarca = 4, Nombre = "Seven seven" });
+lista_marcas.Add(new Marcas() { IdMarcas = 1, Nombre = "Arturo Calle", Nit = 123 });
+lista_marcas.Add(new Marcas() { IdMarcas = 2, Nombre = "Patprimo", Nit = 456 });
+lista_marcas.Add(new Marcas() { IdMarcas = 3, Nombre = "Estudio F", Nit = 101 });
+
 
 
 var lista_productos = new List<Productos>();
 
-lista_productos.Add(new Productos() { IdProducto = 1, Nombre = "Blazer", Material = "Algodón", ValorUnitario = 300000, _Marca = lista_marcas[0] });
-lista_productos.Add(new Productos() { IdProducto = 2, Nombre = "Camisa", Material = "Seda", ValorUnitario = 400000, _Marca = lista_marcas[1] });
-lista_productos.Add(new Productos() { IdProducto = 3, Nombre = "Polo", Material = "Poliester", ValorUnitario = 100000, _Marca = lista_marcas[1] });
-lista_productos.Add(new Productos() { IdProducto = 4, Nombre = "Blusa", Material = "Algodón", ValorUnitario = 500000, _Marca = lista_marcas[2] });
-lista_productos.Add(new Productos() { IdProducto = 5, Nombre = "Chaleco", Material = "Algodón", ValorUnitario = 200000, _Marca = lista_marcas[2] });
-lista_productos.Add(new Productos() { IdProducto = 6, Nombre = "Cardigan", Material = "Poliester", ValorUnitario = 150000, _Marca = lista_marcas[3] });
+lista_productos.Add(new Productos() { IdProductos = 1, Nombre = "Blazer", Material = "Algodón", ValorUnitario = 300000, _Marca = lista_marcas[0] });
+lista_productos.Add(new Productos() { IdProductos = 2, Nombre = "Camisa", Material = "Seda", ValorUnitario = 400000, _Marca = lista_marcas[1] });
+lista_productos.Add(new Productos() { IdProductos = 3, Nombre = "Blusa", Material = "Algodon", ValorUnitario = 500000, _Marca = lista_marcas[2] });
+
 
 
 var lista_compras = new List<Compras>();
 
 lista_compras.Add(new Compras() { IdCompras = 1, Codigo = "A001", Fecha = new DateTime( 2025, 2, 1), ValorTotal = 600000, _Cliente = lista_clientes[0], _Lugar = lista_lugares[0], _MetodoPago = lista_metodospagos[1], _Sucursal = lista_sucursales[0] });
-lista_compras.Add(new Compras() { IdCompras = 2, Codigo = "B001", Fecha = new DateTime( 2025, 1, 25), ValorTotal = 1300000, _Cliente = lista_clientes[1], _Lugar = lista_lugares[1], _MetodoPago = lista_metodospagos[0], _Sucursal = lista_sucursales[1] });
-lista_compras.Add(new Compras() { IdCompras = 3, Codigo = "C003", Fecha = new DateTime( 2025, 1, 19), ValorTotal = 2000000, _Cliente = lista_clientes[2], _Lugar = lista_lugares[0], _MetodoPago = lista_metodospagos[2], _Sucursal = lista_sucursales[0] });
-lista_compras.Add(new Compras() { IdCompras = 4, Codigo = "D004", Fecha = new DateTime( 2025, 2, 1), ValorTotal = 1300000, _Cliente = lista_clientes[3], _Lugar = lista_lugares[2], _MetodoPago = lista_metodospagos[1], _Sucursal = lista_sucursales[2] });
-lista_compras.Add(new Compras() { IdCompras = 5, Codigo = "Z001", Fecha = new DateTime( 2025, 1, 5), ValorTotal = 450000, _Cliente = lista_clientes[4], _Lugar = lista_lugares[1], _MetodoPago = lista_metodospagos[2], _Sucursal = lista_sucursales[1] });
-lista_compras.Add(new Compras() { IdCompras = 6, Codigo = "Y002", Fecha = new DateTime( 2025, 5, 1), ValorTotal = 300000, _Cliente = lista_clientes[5], _Lugar = lista_lugares[1], _MetodoPago = lista_metodospagos[2], _Sucursal = lista_sucursales[3] });
+lista_compras.Add(new Compras() { IdCompras = 2, Codigo = "B001", Fecha = new DateTime( 2025, 1, 25), ValorTotal = 1200000, _Cliente = lista_clientes[1], _Lugar = lista_lugares[1], _MetodoPago = lista_metodospagos[0], _Sucursal = lista_sucursales[1] });
+lista_compras.Add(new Compras() { IdCompras = 3, Codigo = "C003", Fecha = new DateTime( 2025, 1, 19), ValorTotal = 2000000, _Cliente = lista_clientes[2], _Lugar = lista_lugares[0], _MetodoPago = lista_metodospagos[2], _Sucursal = lista_sucursales[3] });
+lista_compras.Add(new Compras() { IdCompras = 4, Codigo = "D004", Fecha = new DateTime( 2025, 2, 1), ValorTotal = 500000, _Cliente = lista_clientes[3], _Lugar = lista_lugares[2], _MetodoPago = lista_metodospagos[1], _Sucursal = lista_sucursales[2] });
+
 
 
 var lista_detallescompras = new List<DetallesCompras>();
 
 lista_detallescompras.Add(new DetallesCompras() {IdDetalles = 1, Cantidad = 2, ValorBruto = 600000, _Compra = lista_compras[0], _Producto = lista_productos[0] });
 lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 2, Cantidad = 3, ValorBruto = 1200000, _Compra = lista_compras[1], _Producto = lista_productos[1] });
-lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 3, Cantidad = 1, ValorBruto = 100000, _Compra = lista_compras[1], _Producto = lista_productos[2] });
-lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 4, Cantidad = 5, ValorBruto = 2000000, _Compra = lista_compras[2], _Producto = lista_productos[1] });
-lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 5, Cantidad = 1, ValorBruto = 500000, _Compra = lista_compras[3], _Producto = lista_productos[3] });
-lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 6, Cantidad = 4, ValorBruto = 800000, _Compra = lista_compras[3], _Producto = lista_productos[4] });
-lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 7, Cantidad = 3, ValorBruto = 450000, _Compra = lista_compras[4], _Producto = lista_productos[5] });
-lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 8, Cantidad = 1, ValorBruto = 300000, _Compra = lista_compras[5], _Producto = lista_productos[0] });
+lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 3, Cantidad = 5, ValorBruto = 2000000, _Compra = lista_compras[2], _Producto = lista_productos[1] });
+lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 4, Cantidad = 1, ValorBruto = 500000, _Compra = lista_compras[3], _Producto = lista_productos[2] });
+
+;
 
 
 
@@ -80,9 +74,9 @@ lista_detallescompras.Add(new DetallesCompras() { IdDetalles = 8, Cantidad = 1, 
 public class Clientes
 {
 
-    public int IdCliente { get; set; }
+    public int IdClientes { get; set; }
+    public string? Cedula { get; set; }
     public string? Nombre { get; set; }
-    public string? Apellido { get; set; }
     public string? Telefono { get; set; }
 
     public List<Compras> Compras { get; set; }
@@ -95,6 +89,7 @@ public class Sucursales
 
     public int IdSucursales { get; set; }
     public string? Nombre { get; set;}
+    public string? Direccion { get; set; }  
 
     public List<Compras> Compras { get; set; }
 
@@ -103,8 +98,9 @@ public class Sucursales
 public class MetodosPagos
 {
 
-    public int IdMetodoPago { get; set; }
+    public int IdMetodosPagos { get; set; }
     public string? Nombre { get;set; }
+    public Boolean? Estado { get; set; }
 
 
     public List<Compras> Compras { get; set; }
@@ -114,8 +110,9 @@ public class MetodosPagos
 public class Lugares
 {
 
-    public int IdLugar { get; set; }
-    public string Nombre { get; set; }
+    public int IdLugares { get; set; }
+    public string? Nombre { get; set; }
+    public string? CodigoPostal { get; set; }
 
 
     public List<Compras> Compras { get; set; }
@@ -126,8 +123,9 @@ public class Lugares
 public class Marcas
 {
 
-    public int IdMarca { get; set; }
-    public string Nombre { get; set; }
+    public int IdMarcas { get; set; }
+    public string? Nombre { get; set; }
+    public int Nit {  get; set; }
 
 
     public List<Productos> Productos { get; set; }
@@ -137,10 +135,13 @@ public class Marcas
 public class Productos
 {
 
-    public int IdProducto { get; set; }
+    public int IdProductos { get; set; }
     public string? Nombre { get; set; }
     public string? Material { get; set; }
     public double ValorUnitario { get; set; }
+
+    //FK
+    public int IdMarcas { get; set; }   
 
 
     public Marcas? _Marca { get; set; }
@@ -158,6 +159,12 @@ public class Compras
     public string? Codigo { get; set; }
     public double ValorTotal { get; set; }
 
+    //FK
+    public int IdClientes { get; set; }
+    public int IdSucursales { get; set; }
+    public int IdMetodosPagos { get; set; }
+    public int IdLugares { get; set; }
+
 
     public Clientes? _Cliente { get; set; }
     public Sucursales? _Sucursal { get; set; }
@@ -174,6 +181,10 @@ public class DetallesCompras
     public int IdDetalles { get; set; }
     public int Cantidad { get; set; }
     public double ValorBruto { get; set; }
+
+    //FK
+    public int IdCompras { get; set; }
+    public int IdProductos { get; set; }
 
 
     public Compras? _Compra { get; set; }
