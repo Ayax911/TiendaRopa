43210246c03ba0b3b5b1bc7b79c351b5a827d8fb
TiendaRopa <j.mast8924@gmail.com>
2025-03-30 13:19:22 -0500

    Reconfirmacion subida

---
+++ ut_productos/Nucleo/EntidadesNucleo.cs
@@ -0,0 +1,23 @@
+﻿
+using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+
+namespace ut_clientes.Nucleo
+{
+    public class EntidadesNucleo
+    {
+        public static Productos? Productos(IConexion conexion)
+        {
+            var marca = conexion.Marcas!.FirstOrDefault(x => x.Nombre == "RopaFina");
+
+            var entidad = new Productos();
+            entidad.Nombre = "ArturoCalle";
+            entidad.Material = "Lino";
+            entidad.ValorUnitario = 100000;
+            entidad.Marcas = marca!.Id; 
+
+            return entidad;
+        }
+    }
+}
