1278dc80f13d4418302ba5d95552a76d17a2ab6d
TiendaRopa <j.mast8924@gmail.com>
2025-03-29 20:22:58 -0500

    Agregar archivos de proyecto.

---
+++ lib_dominio/Entidades/Marcas.cs
@@ -0,0 +1,17 @@
+﻿
+namespace lib_dominio.Entidades
+{
+
+    public class Marcas
+    {
+
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public int Nit { get; set; }
+
+
+        public List<Productos>? Productos { get; set; }
+
+    }
+
+}
---
+++ lib_dominio/Entidades/Productos.cs
@@ -0,0 +1,24 @@
+﻿
+namespace lib_dominio.Entidades
+{
+    public class Productos
+    {
+
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Material { get; set; }
+        public double ValorUnitario { get; set; }
+
+        //FK
+        public int Marcas { get; set; }
+
+
+        public Marcas? _Marca { get; set; }
+
+
+        public List<DetallesCompras>? DetallesCompras { get; set; }
+
+    }
+
+
+}
