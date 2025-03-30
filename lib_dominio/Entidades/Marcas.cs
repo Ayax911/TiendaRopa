6ef8ede7c060a257981e5111f676053a808e45be
TiendaRopa <j.mast8924@gmail.com>
2025-03-30 11:15:04 -0500

    Pruebas unitarias "Marcas".
    -Modificacion script.sql
    -Creacion e implementacion del
    ut_marcas

--- lib_dominio/Entidades/Marcas.cs
+++ lib_dominio/Entidades/Marcas.cs
@@ -7,7 +7,7 @@ namespace lib_dominio.Entidades
 
         public int Id { get; set; }
         public string? Nombre { get; set; }
-        public int Nit { get; set; }
+        public string? Nit { get; set; }
 
 
         public List<Productos>? Productos { get; set; }
