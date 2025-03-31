4524b854b5179ac1918331dbf935f7f3ce175084
LuisMiguelQuintero <luisqg.2012@gmail.com>
2025-03-31 00:47:12 -0500

    Se cambio nombre de archivo ut_proveedores a ut_marcas

---
+++ ut_marcas/Nucleo/EntidadesNucleo.cs
@@ -0,0 +1,17 @@
+﻿
+using lib_dominio.Entidades;
+
+namespace ut_clientes.Nucleo
+{
+    public class EntidadesNucleo
+    {
+        public static Marcas? Marcas()
+        {
+            var entidad = new Marcas();
+            entidad.Nit = "222222";
+            entidad.Nombre = "Marca Prueba 2";
+            
+            return entidad;
+        }
+    }
+}
